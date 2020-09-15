5854796d87b5f181711cda2af7621024381632a3
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-09-15 10:19:54 -0500

    render picture gallery

---
+++ src/PictureGallery.js
@@ -0,0 +1,24 @@
+// Import React Library
+// And also the `Component` variable from inside it
+import React, { Component } from 'react';
+
+class PictureGallery extends Component {
+    render() {
+        return (
+            <div>
+                <img src="https://picsum.photos/200/200" />
+                <img src="https://picsum.photos/200/200" />
+                <img src="https://picsum.photos/200/200" />
+                <img src="https://picsum.photos/200/200" />
+                <img src="https://picsum.photos/200/200" />
+                <img src="https://picsum.photos/200/200" />
+            </div>
+        );
+    }
+}
+
+// If this were node...
+// module.exports = PictureGallery;
+
+// But on the client-side we do:
+export default PictureGallery;
